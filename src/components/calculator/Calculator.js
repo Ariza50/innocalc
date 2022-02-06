d99547ccd958cc1cea6ea37390bd8def5b0cfec9
Vicente Albert <vicente.albert@lambdaloopers.com>
2022-02-06 18:42:55 +0100

    functionalitty to +, -, / and * operations

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,31 +1,143 @@
 import './Calculator.css';
+import {useState} from 'react';
+import Button from './components/Button/Button';
 
 const Calculator = () => {
+  let [calculator, setCalculator] = useState({
+    displayValue: 0,
+    firstValue: 0,
+    secondValue: 0,
+    operator: null,
+    operated: false,
+  })
 
+  const resetValues = () => {
+    setCalculator({
+      displayValue: 0,
+      firstValue: 0,
+      secondValue: 0,
+      operator: null,
+    });
+  }
+
+  const invertSign = () => {
+    setCalculator({
+      ...calculator,
+      displayValue: calculator.displayValue * -1
+    });
+  }
+
+  const numberClick = (newNumber) => {
+    let value;
+    let key;
+    let operated;
+    if (calculator.operated) {
+      key = 'firstValue'
+      value = newNumber;
+      operated = false;
+    } else {
+      key = calculator.operator ? 'secondValue' : 'firstValue';
+      value = parseFloat(String(calculator[key]) + newNumber);
+      operated = calculator.operated;
+    }
+
+    setCalculator({
+      ...calculator,
+      [key]: value,
+      displayValue: value.toString(10),
+      operated
+    })
+  }
+
+  const updateOperation = () => {
+    if (!calculator.operator) {
+      return;
+    }
+
+    const result = calculator.operator === '+'
+      ? calculator.firstValue + calculator.secondValue
+      : calculator.operator === '-'
+      ? calculator.firstValue - calculator.secondValue
+      : calculator.operator === '×'
+      ? calculator.firstValue * calculator.secondValue
+      : calculator.operator === '÷'
+      ? calculator.firstValue / calculator.secondValue
+      : 0
+
+    setCalculator({
+      displayValue: String(result),
+      firstValue: result,
+      secondValue: 0,
+      operator: null,
+      operated: true,
+    });
+  }
+
+  const addOperand = (operand) => {
+    console.log('addOperand ', operand);
+    setCalculator({
+      ...calculator,
+      operator: operand
+    });
+    // updateOperation();
+  }
+
+  const buttonValues = {
+    AC: resetValues,
+    '+/-': invertSign,
+    '÷': addOperand,
+    '×': addOperand,
+    '-': addOperand,
+    '+': addOperand,
+    '=': updateOperation,
+  }
+
+  const checkButton = (value) => {
+    buttonValues[value]();
+  }
+  console.log('calculator ', calculator);
   return (
     <div className="calculator-container">
-      <header>0</header>
+      <header>{calculator.displayValue}</header>
       {/*<input type="text" value={0} />*/}
       <div className="keyboard-container">
-        <button className="special">AC</button>
-        <button className="special">+/-</button>
-        <button className="special">%</button>
-        <button className="operator">÷</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="operator">×</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className="operator">-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="operator">+</button>
-        <button>0</button>
-        <button>,</button>
-        <button className="operator extra">=</button>
+        <Button value="AC" className="special" onClick={() => checkButton('AC')} />
+        {/*<button className="special">AC</button>*/}
+        <Button value="+/-" className="special" onClick={() => checkButton('+/-')} />
+        {/*<button className="special">+/-</button>*/}
+        <Button value="%" className="special" onClick={() => checkButton('%')} />
+        {/*<button className="special">%</button>*/}
+        <Button value="÷" className="operator" onClick={() => addOperand('÷')} />
+        {/*<button className="operator">÷</button>*/}
+        <Button value="7" onClick={() => numberClick('7')} />
+        {/*<button>7</button>*/}
+        <Button value="8" onClick={() => numberClick('8')} />
+        {/*<button>8</button>*/}
+        <Button value="9" onClick={() => numberClick('9')} />
+        {/*<button>9</button>*/}
+        <Button value="×" className="operator" onClick={() => addOperand('×')} />
+        {/*<button className="operator">×</button>*/}
+        <Button value="4" onClick={() => numberClick('4')} />
+        {/*<button>4</button>*/}
+        <Button value="5" onClick={() => numberClick('5')} />
+        {/*<button>5</button>*/}
+        <Button value="6" onClick={() => numberClick('6')} />
+        {/*<button>6</button>*/}
+        <Button value="-" className="operator" onClick={() => addOperand('-')} />
+        {/*<button className="operator">-</button>*/}
+        <Button value="1" onClick={() => numberClick('1')} />
+        {/*<button>1</button>*/}
+        <Button value="2" onClick={() => numberClick('2')} />
+        {/*<button>2</button>*/}
+        <Button value="3" onClick={() => numberClick('3')} />
+        {/*<button>3</button>*/}
+        <Button value="+" className="operator" onClick={() => addOperand('+')} />
+        {/*<button className="operator">+</button>*/}
+        <Button value="0" onClick={() => numberClick('0')} />
+        {/*<button>0</button>*/}
+        <Button value="," onClick={() => checkButton(',')} />
+        {/*<button>,</button>*/}
+        <Button value="=" className="operator extra" onClick={() => updateOperation()} />
       </div>
     </div>
   );
